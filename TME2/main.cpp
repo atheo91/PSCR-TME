933464be8f6da4258a37140aed51b5338ad5cf1f
necrosan89 <114225661+atheo91@users.noreply.github.com>
2022-10-07 00:06:46 +0200

    TME2 v1.2
    Question 1,2,3,4 faites. Fixer la complexité tros grande de map avec une condition pour sortir de la boucle plus tôt

--- TME2/main.cpp
+++ TME2/main.cpp
@@ -3,13 +3,13 @@
 #include <regex>
 #include <chrono>
 #include <vector>
-#include <hash_map>
+#include "Hash_Map.h"
 
 /*
-Question 1/
-Question 2/
-Question 3/
-Question 4/ Tros complexe, impossible de faire des test;
+Question 1/566193 mots totale
+Question 2/20333 mots différents
+Question 3/0 toto, 114 peace, 298 war.
+Question 4/ Tros complexe. On pourrais faire des table de Hash
 */
 
 int main() {
@@ -25,35 +25,51 @@ int main() {
 	size_t nombre_lu = 0;
 	size_t n_nrepeat = 0;
 	bool w_new = true;
+	int c_war = 0, c_peace = 0, c_toto = 0;
+
 	
 	// prochain mot lu
 	string word;
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
 	regex re(R"([^a-zA-Z])");
+
+
 	while (input >> word) {
-		w_new = true;
+		
 		// élimine la ponctuation et les caractères spéciaux
 		word = regex_replace(word, re, "");
 		// passe en lowercase
 		transform(word.begin(), word.end(), word.begin(), ::tolower);
 		// word est maintenant "tout propre"
 
+
+		w_new = true;
 		//Parcours toutes la table a chaque nouveaux mots pour savoir si il est nouveaux
-		for (auto i = liste.begin(); i != liste.end(); i++) {
+		for (auto i = liste.begin(); i != liste.end() && w_new != false; i++) {
 			if (i->first == word) {
 				w_new = false;
 				++i->second;
 			}
 		}
 
+		if (word == "toto") {
+			++c_toto;
+		}
+		else if (word == "war") {
+			++c_war;
+		}
+		else if (word == "peace") {
+			++c_peace;
+		}
+
 		//Si c'est un nouveau mots.
 		if (w_new == true) {
 			//On l'ajoute a la liste
 			liste.push_back({ word,1 });		
-			/*if (n_nrepeat % 1000 == 0) {
+			if (n_nrepeat % 1000 == 0) {
 				// on affiche un mot "propre" sur 1000
-				cout << nombre_lu << ": " << liste.at(n_nrepeat) << endl;
-			}*/
+				cout << nombre_lu << ": " << liste.at(n_nrepeat).first << endl;
+			}
 			n_nrepeat++;
 		}
 		nombre_lu++;
@@ -68,7 +84,10 @@ int main() {
 		<< "ms.\n";
 
 	cout << "Found a total of " << nombre_lu << " words." << endl;
-	cout << "And a total of " << nombre_lu << " unrepeated words." << endl;
-	cout << "And a total of " << nombre_lu << " unrepeated words." << endl;
+	cout << "And a total of " << n_nrepeat << " unrepeated words." << endl;
+	cout << "Number of toto: " << c_toto <<  endl;
+	cout << "Number of peace:" << c_peace << endl;
+	cout << "Number of war: " << c_war << endl;
+	
 	return 0;
 }
